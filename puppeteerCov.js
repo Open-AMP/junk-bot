57749bcc84727fcb8c0afd382b9dc888987766d3
Karan Sapolia <sapoliakaran@gmail.com>
2018-05-04 08:52:21 +0530

    Status messages added to coverage

--- puppeteerCov.js
+++ puppeteerCov.js
@@ -1,11 +1,14 @@
+console.log('Running...');
 const puppeteer = require('/home/x/.nvm/versions/node/v9.5.0/lib/node_modules/puppeteer');
 var fs = require('fs');
 
 var url="https://"+(process.argv[2]).toString();
 var name=url.split('//')[1].split(".")[0];
+console.log(`Extracting Coverage for ${url}`);
 
 (async () => {
-const browser = await puppeteer.launch({executablePath: '/usr/bin/chromium', headless: false});
+console.log(`Opening page in background browser context: /usr/bin/chromium`);
+const browser = await puppeteer.launch({executablePath: '/usr/bin/chromium', headless: true});
 const page = await browser.newPage();
 
 await Promise.all([
@@ -24,4 +27,7 @@ let totalBytes = 0;
 let usedBytes = 0;
 const coverage = [...jsCoverage, ...cssCoverage];
 fs.writeFileSync(`${name}-Cov.json`, JSON.stringify(coverage, null, 2));
+console.log(` -- Coverage data saved to file ${name}-Cov.json. Take a look -- `);
+await browser.close();
+console.log('Closing session...');
 })();
